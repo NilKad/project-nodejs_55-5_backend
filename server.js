e5eba48850c5b3d517c6fd99d195731621616051
NilKad <kadulin@gmail.com>
2023-03-17 17:48:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const { PORT = 80, DB_HOST, DB_TEST_HOST } = process.env;
 // console.log('DB_HOST:\t', DB_HOST);
 // console.log('DB_TEST_HOST:\t', DB_TEST_HOST);
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () => {
-      // createFolderIsNotExist(TEMP_DIR);
-      console.log('Database connection successful');
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  });
+// mongoose
+// .connect(DB_HOST)
+// .then(() => {
+app.listen(PORT, () => {
+  // createFolderIsNotExist(TEMP_DIR);
+  console.log('Database connection successful');
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
+// })
+// .catch(error => {
+// console.log(error.message);
+// process.exit(1);
+// });
 
 // console.log('server is stated!!!!');
