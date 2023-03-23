76157234050a8f477fdc9f4ebe7d271527d5b688
Ihor <102248912+IhorMal@users.noreply.github.com>
2023-03-23 14:32:39 +0200

    fix search

--- controllers/news.js
+++ controllers/news.js
@@ -4,11 +4,13 @@ const { News } = require("../models");
 
 const news = async (req, res, next) => {
 try {
-    
+    if (req.query.search) {
     const search = req.query.search;
     const news = await News.find({ 'title': { $regex: search } });
+    return res.status(200).json(news)
+    }
+    const news = await News.find();
     res.status(200).json(news)
-
 } catch (err) {
     const error = new Error(err.message)
     error.status = 400 
