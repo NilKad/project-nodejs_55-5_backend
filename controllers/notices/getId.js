7fc2dc71de69ca0294388ee73e53cae7690015fd
NilKad <kadulin@gmail.com>
2023-03-23 21:45:19 +0200

    my fix

--- controllers/notices/getId.js
+++ controllers/notices/getId.js
@@ -1,13 +1,15 @@
-const { Notices } = require("../../models");
-
+const { Notices } = require('../../models');
 
 const getId = async (req, res, next) => {
-    const {search, isFavorites, myAdds, findtext} = req.query
-    const id = req.params.id
-   
-    const notices = await Notices.findById({_id: id});
-    
-    res.status(200).json(notices)
-}
+  const { search, isFavorites, myAdds, findtext } = req.query;
+  const id = req.params.id;
+
+  const notices = await Notices.findById({ _id: id }).populate(
+    'owner',
+    '_id email'
+  );
+
+  res.status(200).json(notices);
+};
 
 module.exports = getId;
