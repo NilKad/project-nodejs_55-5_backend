8a4dc1f4781e956e751b2ab834a3720e76cccde1
Ihor <102248912+IhorMal@users.noreply.github.com>
2023-03-23 14:29:43 +0200

    search news

--- controllers/news.js
+++ controllers/news.js
@@ -1,10 +1,14 @@
+const { types } = require("joi");
 const { News } = require("../models");
 
 
 const news = async (req, res, next) => {
 try {
-    const news = await News.find();
+    
+    const search = req.query.search;
+    const news = await News.find({ 'title': { $regex: search } });
     res.status(200).json(news)
+
 } catch (err) {
     const error = new Error(err.message)
     error.status = 400 
