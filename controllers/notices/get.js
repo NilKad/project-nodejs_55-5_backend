4762863203055316ad316b9d41afa11bf12be7fa
NilKad <kadulin@gmail.com>
2023-03-24 18:28:27 +0200

    fix midlware

--- controllers/notices/get.js
+++ controllers/notices/get.js
@@ -1,44 +1,61 @@
-const { Notices } = require("../../models");
+const { Notices } = require('../../models');
 const createError = require('http-errors');
 
 const get = async (req, res, next) => {
-    const {search, isFavorites, myAdds, findtext} = req.query
-    const category = req.params.category
-   
-    if(req.user) {
-        const {_id} = req.user
-        if (isFavorites === 'true' &&  myAdds === 'true') {
-            const err = createError(404, 'The selected category is incorrect');
-            throw err;
-        }
-        console.log(req.user.favorites[0])
-        if (isFavorites === 'true') {
-            if(findtext) {
-                console.log(findtext)
-                const result = await Notices.find({category, _id: { $in: req.user.favorites } , 'title': { $regex: findtext, $options: "i" }})
-                return res.status(200).json(result)
-            }
-          const result = await Notices.find({category, _id: { $in: req.user.favorites } })
-          return res.status(200).json(result)
-        }  
-   
-        if (myAdds === 'true') {
-            if(findtext) {
-                const result = await Notices.find({category, owner: _id, 'title': { $regex: findtext, $options: "i" }})
-                return res.status(200).json(result)
-            }
-          const result = await Notices.find({category, owner: _id})
-          return res.status(200).json(result)
-        }
+  const { search, isFavorites, myAdds, findtext } = req.query;
+  //   console.log('!!!!!!!!', typeof isFavorites);
+  const category = req.params.category;
+
+  if (req.user) {
+    const { _id } = req.user;
+    if (isFavorites === 'true' && myAdds === 'true') {
+      const err = createError(404, 'The selected category is incorrect');
+      throw err;
     }
-  
-    if (findtext) {
-        const notices = await Notices.find({category: category, 'title': { $regex: findtext, $options: "i" }})
-        res.status(200).json(notices)
-    } else {
-        const notices = await Notices.find({'category': {$regex: category, $options: "i"}})
-        res.status(200).json(notices)
-    };
-}
+    console.log(req.user.favorites[0]);
+    if (isFavorites === 'true') {
+      if (findtext) {
+        console.log(findtext);
+        const result = await Notices.find({
+          category,
+          _id: { $in: req.user.favorites },
+          title: { $regex: findtext, $options: 'i' },
+        });
+        return res.status(200).json(result);
+      }
+      const result = await Notices.find({
+        category,
+        _id: { $in: req.user.favorites },
+      });
+      return res.status(200).json(result);
+    }
+
+    if (myAdds === 'true') {
+      if (findtext) {
+        const result = await Notices.find({
+          category,
+          owner: _id,
+          title: { $regex: findtext, $options: 'i' },
+        });
+        return res.status(200).json(result);
+      }
+      const result = await Notices.find({ category, owner: _id });
+      return res.status(200).json(result);
+    }
+  }
+
+  if (findtext) {
+    const notices = await Notices.find({
+      category: category,
+      title: { $regex: findtext, $options: 'i' },
+    });
+    res.status(200).json(notices);
+  } else {
+    const notices = await Notices.find({
+      category: { $regex: category, $options: 'i' },
+    });
+    res.status(200).json(notices);
+  }
+};
 
 module.exports = get;
--- middleWares/tokenValidation.js
+++ middleWares/tokenValidation.js
@@ -1,31 +1,30 @@
 const jwt = require('jsonwebtoken');
 const Users = require('../models/users');
 const createError = require('http-errors');
-const { dataFilter, userFieldEnabledReturnList } = require('../helpers');
+const { dataFilter, userMainField } = require('../helpers');
 
 const { SECRET_KEY } = process.env;
 
 const authMiddleware = async (req, res, next) => {
   const { authorization = '' } = req.headers;
 
-
   const [bearer, token] = authorization.split(' ');
-  if(bearer !== 'Bearer') {
- 
-    return  next()
+  if (bearer !== 'Bearer') {
+    return next();
   }
-
+  console.log('!!!!! token validation');
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await Users.findById({ _id: id });
 
-    if(!user || user.authToken !== token) {
-      next()
-      return
+    if (!user || user.authToken !== token) {
+      next();
+      return;
     }
-    const newUser = dataFilter(user, userFieldEnabledReturnList);
+    const newUser = dataFilter(user, userMainField);
     req.user = newUser;
   } catch (error) {
+    log('Error token validation');
     throw error;
   }
   next();
