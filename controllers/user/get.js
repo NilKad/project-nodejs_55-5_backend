3e3aa3cfeed3433858e551525cd77eefd123a308
NilKad <kadulin@gmail.com>
2023-03-24 15:04:49 +0200

    Update get.js

--- controllers/user/get.js
+++ controllers/user/get.js
@@ -7,6 +7,8 @@ const get = async (req, res, next) => {
   const { user } = req;
   const newUser = dataFilter(user, userMainField);
   const pets = await Pet.find({ owner: user._id });
+  // console.log('user id: ', user._id);
+  // console.log('pets: ', pets);
   res.status(200).json({
     code: 200,
     message: 'Successful operation',
