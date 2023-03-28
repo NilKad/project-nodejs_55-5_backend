1c6fe7f5f9ea5aa3adc3aebc9159915fde94d4c2
NilKad <kadulin@gmail.com>
2023-03-28 15:41:54 +0300

    fix add/del favorites

--- controllers/notices/addFavorit.js
+++ controllers/notices/addFavorit.js
@@ -1,14 +1,18 @@
-
-const Users = require("../../models/users");
+const { Users } = require('../../models/users');
 
 const addFavorit = async (req, res, next) => {
-    const {user, params} = req
-   
-  const ddd =  await  Users.updateOne(
-        { _id: user._id },
-        { $addToSet: { favorites: params.id }}
-      );
-    res.status(201).json({"message": "Success added to favorites"})
-}
+  const { user, params } = req;
+  console.log('ctrl addfavorites param: ', params);
+  console.log('ctrl addfavorites user._id: ', user._id);
+  const ddd = await Users.updateOne(
+    { _id: user._id },
+    { $addToSet: { favorites: params.id } }
+  );
+  // const ddd = await Users.findById(
+  //   { _id: user._id },
+  //   { $addToSet: { favorites: params.id } }
+  // );
+  res.status(201).json({ message: 'Success added to favorites' });
+};
 
 module.exports = addFavorit;
--- controllers/notices/daletFavorit.js
+++ controllers/notices/daletFavorit.js
@@ -1,15 +1,15 @@
-const Users = require("../../models/users");
+const { Users } = require('../../models/users');
 
 const daletFavorit = async (req, res, next) => {
-    const {user, params} = req
-   
-  const ddd =  await  Users.updateOne(
-        { _id: user._id },
-        { $pull: { favorites: params.id } }
-      );
+  const { user, params } = req;
 
-      console.log(ddd)
-    res.status(201).json({"message": "Success added to favorites"})
-}
+  const ddd = await Users.updateOne(
+    { _id: user._id },
+    { $pull: { favorites: params.id } }
+  );
+
+  console.log(ddd);
+  res.status(201).json({ message: 'Success deleted from favorites' });
+};
 
 module.exports = daletFavorit;
