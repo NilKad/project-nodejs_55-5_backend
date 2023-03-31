7726fa39f15bcb69905cb6016b220821da8f10d3
NilKad <kadulin@gmail.com>
2023-03-31 14:53:59 +0300

    fix notices sort date

--- controllers/notices/get.js
+++ controllers/notices/get.js
@@ -2,120 +2,126 @@ const { Notices } = require('../../models');
 const { ValidationError, constructorResponse } = require('../../helpers');
 
 const get = async (req, res, next) => {
-try {
-  const isPagination = req.query.page;
-  const {
-    search,
-    isFavorites,
-    myAdds,
-    findtext,
-    page = 1,
-    perPage = isPagination ? 20 : 5000,
-  } = req.query;
-  const limit = perPage * 1;
-  const skip = perPage * (page - 1);
+  try {
+    const isPagination = req.query.page;
+    const {
+      search,
+      isFavorites,
+      myAdds,
+      findtext,
+      page = 1,
+      perPage = isPagination ? 20 : 5000,
+    } = req.query;
+    const limit = perPage * 1;
+    const skip = perPage * (page - 1);
 
-  const category = req.params.category;
-  let total = await Notices.find({ category }).count();
-  let notices = [];
-  const constructorData = {
-    pagination: isPagination,
-    total,
-    perPage,
-    // data: notices,
-    page,
-  };
+    const category = req.params.category;
+    let total = await Notices.find({ category }).count();
+    let notices = [];
+    const constructorData = {
+      pagination: isPagination,
+      total,
+      perPage,
+      // data: notices,
+      page,
+    };
 
-  if (req.user) {
-    const { _id, favorites } = req.user;
+    if (req.user) {
+      const { _id, favorites } = req.user;
 
-    if (category === 'favorite') {
-   
-      if (findtext) {
-        total = await Notices.find({
-          _id: { $in: favorites },
-          title: { $regex: findtext, $options: 'i' },
-        }).count();
-        constructorData.total = total
+      if (category === 'favorite') {
+        if (findtext) {
+          total = await Notices.find({
+            _id: { $in: favorites },
+            title: { $regex: findtext, $options: 'i' },
+          }).count();
+          constructorData.total = total;
 
-        notices = await Notices.find({
-          _id: { $in: favorites },
-          title: { $regex: findtext, $options: 'i' },
-        })
+          notices = await Notices.find({
+            _id: { $in: favorites },
+            title: { $regex: findtext, $options: 'i' },
+          })
+            .limit(limit)
+            .skip(skip)
+            .sort({ createdAt: -1 });
+          return res
+            .status(200)
+            .json(constructorResponse(constructorData, notices));
+        }
+        total = await Notices.find({ _id: { $in: favorites } }).count();
+        constructorData.total = total;
+        notices = await Notices.find({ _id: { $in: favorites } })
           .limit(limit)
-          .skip(skip);
+          .skip(skip)
+          .sort({ createdAt: -1 });
         return res
           .status(200)
           .json(constructorResponse(constructorData, notices));
       }
-      total = await Notices.find({ _id: { $in: favorites } }).count();
-      constructorData.total = total
-      notices = await Notices.find({ _id: { $in: favorites } })
+      if (category === 'own') {
+        if (findtext) {
+          total = await Notices.find({
+            owner: _id,
+            title: { $regex: findtext, $options: 'i' },
+          }).count();
+          constructorData.total = total;
+          notices = await Notices.find({
+            owner: _id,
+            title: { $regex: findtext, $options: 'i' },
+          })
+            .limit(limit)
+            .skip(skip)
+            .sort({ createdAt: -1 });
+          return res
+            .status(200)
+            .json(constructorResponse(constructorData, notices));
+        }
+
+        notices = await Notices.find({ owner: _id })
+          .limit(limit)
+          .skip(skip)
+          .sort({ createdAt: -1 });
+
+        return res
+          .status(200)
+          .json(constructorResponse(constructorData, notices));
+      }
+    }
+    // if('lost-found' === category || 'for-free' === category ||) {
+
+    // }
+
+    if (findtext) {
+      total = await Notices.find({
+        category: category,
+        title: { $regex: findtext, $options: 'i' },
+      }).count();
+      constructorData.total = total;
+      notices = await Notices.find({
+        category: category,
+        title: { $regex: findtext, $options: 'i' },
+      })
         .limit(limit)
-        .skip(skip);
-      return res
-        .status(200)
-        .json(constructorResponse(constructorData, notices));
-    }
-    if (category === 'own') {
-      if (findtext) {
-        total = await Notices.find({
-          owner: _id,
-          title: { $regex: findtext, $options: 'i' },
-        }).count();
-        constructorData.total = total
-        notices = await Notices.find({
-          owner: _id,
-          title: { $regex: findtext, $options: 'i' },
-        })
-          .limit(limit)
-          .skip(skip);
+        .skip(skip)
+        .sort({ createdAt: -1 });
+      if (isPagination) {
         return res
           .status(200)
           .json(constructorResponse(constructorData, notices));
       }
-
-      notices = await Notices.find({ owner: _id }).limit(limit).skip(skip);
-
-      return res
-        .status(200)
-        .json(constructorResponse(constructorData, notices));
+      res.status(200).json(constructorResponse(constructorData, notices));
+    } else {
+      notices = await Notices.find({
+        category: { $regex: category, $options: 'i' },
+      })
+        .limit(limit)
+        .skip(skip)
+        .sort({ createdAt: -1 });
+      res.status(200).json(constructorResponse(constructorData, notices));
     }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid search characters' });
   }
-  // if('lost-found' === category || 'for-free' === category ||) {
-
-  // }
-
-  if (findtext) {
-    total = await Notices.find({
-      category: category,
-      title: { $regex: findtext, $options: 'i' },
-    }).count();
-    constructorData.total = total
-    notices = await Notices.find({
-      category: category,
-      title: { $regex: findtext, $options: 'i' },
-    })
-      .limit(limit)
-      .skip(skip);
-    if (isPagination) {
-      return res
-        .status(200)
-        .json(constructorResponse(constructorData, notices));
-    }
-    res.status(200).json(constructorResponse(constructorData, notices));
-  } else {
-    notices = await Notices.find({
-      category: { $regex: category, $options: 'i' },
-    })
-      .limit(limit)
-      .skip(skip);
-    res.status(200).json(constructorResponse(constructorData, notices));
-  }
-
-} catch (error) {
-  res.status(400).json({message: 'Invalid search characters'});
-}
   // const isPagination = req.query.page;
   // const {
   //   search,
